776a6f1fd669d66cf6340bcfc7eb5f96c16746b0
samdhein <33975663+samdhein@users.noreply.github.com>
2022-02-13 22:30:24 -0800

    assignment complete

---
+++ src/test/java/com/samhein/hoppersreceipt/HoppersreceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samhein.hoppersreceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersreceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
